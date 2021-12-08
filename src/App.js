c91e709e7d6591dd8802025d9d1aefccd39bc8d1
poletko-admin <onewebdesignpl@gmail.com>
2021-12-08 19:09:30 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from "react-router-dom";
 
-function App() {
+import Dashboard from "./pages/Dashboard";
+import Articles from "./pages/Articles";
+import React from "react";
+
+const App = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div>dd</div>
+      <BrowserRouter></BrowserRouter>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Articles.js
@@ -0,0 +1,9 @@
+const Articles = (props) => {
+  return (
+    <div>
+      <p>Articles</p>
+    </div>
+  );
+};
+
+export default Articles;
